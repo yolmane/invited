79a9149237d3f0e654d63735ebae2d41c47fff03
Сергей <yolmane@yandex.ru>
2024-06-02 17:25:09 +0300

    Initial commit

---
+++ scripts/index.js
@@ -0,0 +1,24 @@
+// 2345678910111213141516171819202122
+// document.addEventListener('DOMContentLoaded', function() {
+//     let block = document.querySelectorAll('.block__day-programm');
+ 
+//     function checkBlocksVisibility() {
+//         let windowHeight = window.innerHeight;
+ 
+//         const newLocal = & gt;
+//         block.forEach(block__day-programm =newLocal; {
+//             let blockPosition = block.getBoundingClientRect().top;
+ 
+//             if (blockPosition &lt; windowHeight - 100) {
+//                 block.style.opacity = "1";
+//                 block.style.transform = "translateY(0)";
+//             }
+//         });
+//     }
+ 
+//     checkBlocksVisibility();
+ 
+//     window.addEventListener('scroll', function() {
+//         checkBlocksVisibility();
+//     });
+// });
